d926dbd1662ae588b0c84fbed058f3d4503ef6e8
Masa Lubbadeh <masa.lubbadeh@hotmail.com>
2024-11-17 17:10:48 +0200

    created replicas

--- catalogServer/catalog.js
+++ catalogServer/catalog.js
@@ -9,25 +9,26 @@ app.get("/query", (req, res) => {
   const { topic, id } = req.query;
 
   if (id) {
+    lkjfdbg
     const book = getBookInfo(id);
     if (book) {
-        return res.status(200).json({
-            title: book.title,
-            quantity: book.quantity,
-            price: book.price
-        });
+      return res.status(200).json({
+        title: book.title,
+        quantity: book.quantity,
+        price: book.price
+      });
     } else {
-        return res.status(404).json({ error: "Book not found" });
+      return res.status(404).json({ error: "Book not found" });
     }
   }
 
   if (topic) {
     // Search by topic
     const booksByTopic = getBooksByTopic(topic);
-    
+
     // map to return only id and title for each book
     const returnedBooks = booksByTopic.map(book => ({
-      id: book.id, title: book.title 
+      id: book.id, title: book.title
     }));
 
     if (returnedBooks.length === 0) {
@@ -43,11 +44,11 @@ app.post('/handleBookPurchase', (req, res) => {
 
   const book = getBookInfo(id);  // check if the book exists
   if (!book) {
-      return res.status(404).json({ message: 'Book not found' });
+    return res.status(404).json({ message: 'Book not found' });
   }
 
   if (book.quantity <= 0) {  //if the book is out of stock
-      return res.status(400).json({ message: 'Stock is empty' }); // changed to 400 for clarity
+    return res.status(400).json({ message: 'Stock is empty' }); // changed to 400 for clarity
   }
 
   book.quantity -= 1;//stock
